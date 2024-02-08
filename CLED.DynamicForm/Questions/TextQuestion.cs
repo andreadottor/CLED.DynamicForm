4aa924652cc0c73f2987418ca9bfb6dedcb37894
Andrea Dottor <andrea@dottor.net>
2024-02-08 10:28:18 +0100

    Refactoring

--- CLED.DynamicForm/Questions/TextQuestion.cs
+++ CLED.DynamicForm/Questions/TextQuestion.cs
@@ -1,26 +1,23 @@
-﻿using Spectre.Console;
+﻿namespace CLED.DynamicForm.Questions;
 
-namespace CLED.DynamicForm.Questions
+class TextQuestion : IQuestion
 {
-    class TextQuestion : IQuestion
+    public int Id { get; set; }
+    public string? Text { get; set; }
+    public bool IsRequired { get; set; }
+
+
+    public string Ask()
     {
-        public int Id { get; set; }
-        public string? Text { get; set; }
-        public bool IsRequired { get; set; }
-
-
-        public string Ask()
+        Console.WriteLine(Text);
+        var answer = Console.ReadLine();
+        while (IsRequired && string.IsNullOrEmpty(answer))
         {
-            Console.WriteLine(Text);
-            var answer = Console.ReadLine();
-            while (IsRequired && string.IsNullOrEmpty(answer))
-            {
-                Console.WriteLine("la domanda è obbligatoria. fornire l'informazione");
-                answer = Console.ReadLine();
-            }
-
-            return answer ?? string.Empty;
-
+            Console.WriteLine("la domanda è obbligatoria. fornire l'informazione");
+            answer = Console.ReadLine();
         }
+
+        return answer ?? string.Empty;
+
     }
 }
