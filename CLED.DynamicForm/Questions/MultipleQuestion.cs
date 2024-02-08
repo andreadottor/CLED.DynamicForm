751e571a5d7b71bf21ad53ac77693b5d90524de7
Andrea Dottor <andrea@dottor.net>
2024-02-08 11:06:44 +0100

    Refactoring

--- CLED.DynamicForm/Questions/MultipleQuestion.cs
+++ CLED.DynamicForm/Questions/MultipleQuestion.cs
@@ -1,26 +1,24 @@
-using CLED.DynamicForm.Questions;
 using Spectre.Console;
 
-namespace CLED.DynamicForm;
+namespace CLED.DynamicForm.Questions;
 
 class MultipleQuestion : IQuestion
 {
     public int Id { get; set; }
-    
     public string? Text { get; set; }
     public bool IsRequired { get; set; }
-
     public List<string> Answers { get; set; }
 
     public MultipleQuestion()
     {
         Answers = new List<string>();
     }
-    
+
     public string Ask()
     {
-        if(!IsRequired)
-            Answers.Add(String.Empty);
+        if (!IsRequired)
+            Answers.Add(string.Empty);
+
         var answer = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
                 .Title(Text)
