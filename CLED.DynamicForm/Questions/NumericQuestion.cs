86d0ab90e021efe5b8c0dfadf7277044e9aadfd5
yassir oubelkasse <yassir.oubelkasse@stud.itsaltoadriatico.it>
2024-02-08 10:45:34 +0100

    Add numeric question

---
+++ CLED.DynamicForm/Questions/NumericQuestion.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace CLED.DynamicForm.Questions
+{
+    class NumericQuestion : IQuestion
+    {
+        public int Id { get; set; }
+
+        public string? Text { get; set; }
+        public bool IsRequired { get; set; }
+
+        public string Ask()
+        {
+            Console.WriteLine(Text);
+            var answer = Console.ReadLine();
+            var isNumber = int.TryParse(answer, out int number);
+
+            while ((IsRequired && string.IsNullOrEmpty(answer)) || !isNumber)
+            {
+                Console.WriteLine("Rinserire l'input o Dato obbligatorio");
+                answer = Console.ReadLine();
+                isNumber = int.TryParse(answer, out number);
+            }
+
+            return answer ?? string.Empty;
+
+        }
+    }
+}
